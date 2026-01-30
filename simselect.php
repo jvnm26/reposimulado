3c4dcbf388c8a061d213b05b927861aba5f0c450
jvnm26 <joao.victormartinsest@gmail.com>
2026-01-29 21:30:48 -0300

    Merge branch 'main' of https://github.com/jvnm26/reposimulado

--- simselect.php
+++ simselect.php
@@ -5,8 +5,18 @@
     $idp = $_SESSION['id'];
     $sql = "select s.id, s.nome simulado, p.nome professor, s.data data from simulado s
     inner join professor p on p.id = s.idprofessor";
+
     $result = mysqli_query($con, $sql);
+    if (isset($_POST['submit'])) {
+
+      $deleteid = $row['id'];
+      $sql = "INSERT INTO simulado(idprofessor, nome, data) VALUES ('$idp','$nome','$data')";
+      $result = mysqli_query($con, $sql);
+    }
+
+    
 ?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -37,88 +47,76 @@
           Novo Simulado
         </button>
 
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Vamos lá!</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            <input type="text" class="form-control" placeholder="Digite o nome da prova" aria-describedby="basic-addon1">
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não, fechar</button>
-                <button type="button" class="btn btn-primary" id="btniniciar">Iniciar novo
-                  simulado</button>
+        <!-- Modal de INSERT-->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Vamos lá!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+              <input type="text" class="form-control" placeholder="Digite o nome da prova" aria-describedby="basic-addon1" name="nome">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não, fechar</button>
+                  <a href="perselect.php?simid={$row['id']}"><button type="submit" class="btn btn-primary" id="btniniciar" name="submit">Iniciar novo
+                    simulado</button></a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+        <!-- Modal de INSERT -->
+    </form>
+    <form action="simdelete.php" method="post" name="deleteid">
+        <!-- Modal de Delete -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Deseja deletar o Simulado?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <h6>Id:</h6>
+                <input type="text" value=" <?php echo $row['id']?> ">
+                <h6>Matéria:</h6>
+                <h6>Professor:</h6>
+                <h6>Data:</h6>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Não, Fechar </button>
+                <button type="submit" class="btn btn-primary" >Deletar Simulado</button>
+              </div>
             </div>
           </div>
         </div>
         <!-- Modal de Delete -->
       </div>
-    </div>
+    </form>
     <hr>
     <div class="menuItens">
-      <!-- Container de simulado -->
-      <!-- Utilizar como a "linha da tabela" -->
-      <div class="menuContainer">
-        <div class="container-fluid">
-          <table>
-            <thead>
-              <tr>
-                <?php
-                  $lista = ['Id', 'Nome', 'Professora', 'Data', 'Componentes'];
-                  for ($lc=0; $lc < count($lista); $lc++) { 
-                      echo"<th scope='col'>" . $lista[$lc] . "</th>";
-                  }
-                  ?>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-                if($result && mysqli_num_rows($result)>0){
-                while ($row = mysqli_fetch_assoc($result)){
-                $data = !empty($row['data']) ? date("d/m/Y", strtotime($row['data'])) : "-";
-                echo "<tr>
-                <td>{$row['id']}</td>
-                <td>{$row['simulado']}</td>
-                <td>{$row['professor']}</td>
-                <td>{$data}</td>
-                <td>
-                <div class='buttonOptions'>
-                <a href='simupdate.php?updateid={$row['id']}'<button class='btnUpdate'><i class='bi bi-pencil-square'></i></button></a>
-                  <a href='simdelete.php?deleteid={$row['id']}'><button class='btnDelete'><i class='bi bi-trash-fill'></i></button></a>
-                  <button class='btnExecute'><i class='bi bi-play-fill'></i></button>
-                </div>
-                </td>";
-                }
-                  } else {
-                    echo "<tr><td colspan='9' class='text-center'>Nenhuma prova encontrada.</td></tr>";
-                  }
-              ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
+        <?php
+        if($result && mysqli_num_rows($result)>0){
+          while ($row = mysqli_fetch_assoc($result)){
+          $data = !empty($row['data']) ? date("d/m/Y", strtotime($row['data'])) : "-";
+        echo"
+          <div class='menuContainer'>
+                    <div class='simuName'>{$row['simulado']}</div>
+                    <div class='questCount'>{$row['professor']}</div>
+                    <div class='questCount'>{$data}</div>
+                    <div class='buttonOptions'>
+                        <a href='simupdate.php?updateid={$row['id']}'><button class='btnUpdate'><i class='bi bi-pencil-square'></i></button></a>
+                        <button type='button' class='btnDelete' data-bs-toggle='modal' data-bs-target='#staticBackdrop'><i class='bi bi-trash-fill'></i></button>
+                        <a href='perselect.php?simid={$row['id']}'><button class='btnExecute'><i class='bi bi-search'></i></button></a>
+                    </div>
+          </div>
+        ";
+          }
+        } else {
+          echo "<tr><td colspan='9' class='text-center'>Nenhuma prova encontrada.</td></tr>";
+        }
+        ?>
     </div>
   </main>
   <footer>
